032d20432fe756ba2d7c1573912e32eb3dba5ad7
Raja Keshri <rajakeshri411@gmail.com>
2023-03-17 16:13:11 +0530

    Updates

---
+++ precache-manifest.0f4018004b3609280241fba886470492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4b8bfc1dfd3a7c352c1",
+    "url": "/keshri03/TextUtils-React/static/css/main.b94a4a28.chunk.css"
+  },
+  {
+    "revision": "b4b8bfc1dfd3a7c352c1",
+    "url": "/keshri03/TextUtils-React/static/js/main.b4b8bfc1.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/keshri03/TextUtils-React/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/keshri03/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "060cd088f3c980b909eb",
+    "url": "/keshri03/TextUtils-React/static/js/runtime~main.060cd088.js"
+  },
+  {
+    "revision": "c208e650acec194ade1d866730ae38a2",
+    "url": "/keshri03/TextUtils-React/index.html"
+  }
+];
